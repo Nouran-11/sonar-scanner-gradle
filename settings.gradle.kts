bb760bd0ee1126c3b8d6349acb250c931a0f74f9
nouranatef <engineer_nouran@yahoo.com>
2025-08-15 17:05:53 +0300

    update version to 9.0.0-SNAPSHOT and disable Develocity configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,24 +29,24 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version("3.18.2")
+  //  id("com.gradle.develocity") version("3.18.2")
 }
-develocity {
-    server.set("https://develocity.sonar.build")
-    buildScan {
-        capture {
-            buildLogging.set(!startParameter.taskNames.contains("properties"))
-        }
-    }
-}
-val isCI: Boolean = System.getenv("CI") != null
-buildCache {
-    local {
-        isEnabled = !isCI
-    }
-    remote(develocity.buildCache) {
-        isEnabled = true
-        isPush = isCI
-    }
-}
+//develocity {
+//    server.set("https://develocity.sonar.build")
+//    buildScan {
+//        capture {
+//            buildLogging.set(!startParameter.taskNames.contains("properties"))
+//        }
+//    }
+//}
+//val isCI: Boolean = System.getenv("CI") != null
+//buildCache {
+//    local {
+//        isEnabled = !isCI
+//    }
+//    remote(develocity.buildCache) {
+//        isEnabled = true
+//        isPush = isCI
+//    }
+//}
 
